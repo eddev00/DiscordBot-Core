b054429ecfbad5cb127aa1e51578eb8347cca4b4
unknown <eddevjo@gmail.com>
2022-10-29 23:58:26 +0100

    Adding Clear command that deletes chat messages.

---
+++ Commands/Moderation/clear.js
@@ -0,0 +1,60 @@
+const {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  PermissionFlagsBits,
+  EmbedBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Clear a specific amount of messages")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("Amount of messages to clear")
+        .setRequired(true)
+    )
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("Select a target to clear their messages")
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    const { channel, options } = interaction;
+    const amount = options.getInteger("amount");
+    const target = options.getUser("target");
+    const messages = await channel.messages.fetch({
+      limit: amount + 1,
+    });
+
+    const res = new EmbedBuilder().setColor(0x5fb041);
+
+    if (target) {
+      let i = 0;
+      const fitlered = [];
+      (await messages).filter((msg) => {
+        if (msg.author.id === target && amount > i) {
+          fitlered.push(msg);
+          i++;
+        }
+      });
+
+      await channel.bulkDelete(fitlered).then((messages) => {
+        res.setDescription(
+          `Succesfully deleted ${messages.size} messages from ${target}.`
+        );
+        interaction.reply({ embeds: [res], ephermal: true }); // use ephermal or not
+      });
+    } else {
+      await channel.bulkDelete(amount, true).then((messages) => {
+        res.setDescription(
+          `Succesfully deleted ${messages.size} messages from the channel.`
+        );
+        interaction.reply({ embeds: [res], ephermal: true });
+      });
+    }
+  },
+};
